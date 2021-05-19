789716efd706f5672f8c49c96b1c0d3fce975b50
Frederico Neres <neresfrederico@gmail.com>
2021-05-19 08:10:47 -0300

    Alterações para facilitar a leitura no método argumentNotValidException.

--- src/main/java/br/com/zupacademy/frederico/casadocodigo/handler/ArgumentException.java
+++ src/main/java/br/com/zupacademy/frederico/casadocodigo/handler/ArgumentException.java
@@ -2,10 +2,8 @@ package br.com.zupacademy.frederico.casadocodigo.handler;
 
 import br.com.zupacademy.frederico.casadocodigo.handler.dto.ArgumentNotValidExceptionDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,12 +17,9 @@ public class ArgumentException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
   public ArgumentNotValidExceptionDTO argumentNotValidException(MethodArgumentNotValidException exception) {
-        System.out.println("Passa aqui");
         BindingResult bindingResult = exception.getBindingResult();
         List<FieldError> fieldErros = bindingResult.getFieldErrors();
 
-        ArgumentNotValidExceptionDTO argumentNotValidException =
-                new ArgumentNotValidExceptionDTO(fieldErros);
-        return argumentNotValidException;
+        return new ArgumentNotValidExceptionDTO(fieldErros);
     }
 }
